17508eef5be9e9924047ff4b0b21a654fed3e6a7
SUH(green) <suh240505@gmail.com>
2024-05-07 15:53:08 +0900

    Home 헤더,바디,푸터 세 부위로 나눔

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
+import Stack from 'react-bootstrap/Stack';
+
 import GlobalNav from '../components/common/GlobalNav'
 
 function Home(){
     return(
-        <>
-            <GlobalNav/>
-        </>
+        <Stack gap={3}>
+            <div className="p-2"><GlobalNav /></div>
+            <div className="p-2">Second item</div>
+            <div className="p-2">Third item</div>
+        </Stack>
     )
 }
 
