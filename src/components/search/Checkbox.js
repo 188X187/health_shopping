b4360bfe1d7b3211ccb1b75f7a4772e9f6b6c5ab
SUH(green) <suh240505@gmail.com>
2024-05-08 13:10:34 +0900

    성빈-검색 카테고리? 만듦(Filter) + List 손댐

---
+++ src/components/search/Checkbox.js
@@ -0,0 +1,22 @@
+// import { useState } from "react";
+
+// function CheckboxLabel(props){
+// return(
+//     <label>
+//         <input
+//             type="checkbox"
+//             onChange={(e) => {
+//                 if (e.target.checked) {
+//                     let copy = item;
+//                     copy = copy + props.item
+//                     setItem(copy)
+//                 } else {
+//                     setItem('')
+//                 }
+//             }}>
+//         </input>{props.item}
+//     </label>
+// )
+// }
+
+// export default CheckboxLabel
--- src/components/search/Filter.js
+++ src/components/search/Filter.js
@@ -1,24 +1,156 @@
 import { useEffect, useState } from "react";
 import List from "../../pages/List";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Filter() {
+    const [item, setItem] = useState('')
     const [checkbox, setcheckbox] = useState('')
 
     return (
         <>
-        <label>
-            <input
-                type="checkbox"
-                onChange={(e) => {
-                    if(e.target.checked){
-                        setcheckbox("&sort=date")
-                    } else {
-                        setcheckbox('')
-                    }
-                }}>
-            </input>최신순
-        </label>
-        <List checkbox={checkbox}/>
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "유기농"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("유기농", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>유기농
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "다이어트"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("다이어트", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>다이어트
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "게이너"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("게이너", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>게이너
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "아미노산"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("아미노산", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>아미노산
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "다이어트"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("다이어트", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>다이어트
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "에너지"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("에너지", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>에너지
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "쉐이커"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("쉐이커", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>쉐이커
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            let copy = item;
+                            copy = copy + "헬스용품"
+                            setItem(copy)
+                        } else {
+                            let copy = item.replace("헬스용품", "");
+                            setItem(copy)
+                        }
+                    }}>
+                </input>헬스용품
+            </label>
+
+            <label>
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        if (e.target.checked) {
+                            setcheckbox("&sort=date")
+                        } else {
+                            setcheckbox('')
+                        }
+                    }}>
+                </input>최신순
+            </label>
+            <List item={item} checkbox={checkbox} />
         </>
     )
 }
